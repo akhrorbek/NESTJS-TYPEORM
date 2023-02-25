2547735d68b0d921b775479720f15812f904629c
akhrorbek <akhrornarzullayev.na@gmail.com>
2023-02-25 11:51:55 +0500

    init:Added config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { config } from './module/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot(config)],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config'
 
 async function main() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(5454);
+  const config = app.get(ConfigService)
+
+  const host = config.getOrThrow<string>('app.host')
+  const port = config.getOrThrow<number>('app.port')
+
+  await app.listen(port, host);
 }
 main();
